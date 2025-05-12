336e23d2ccfd0834947d3f216e94aca62f567abb
Sayan Hazra <22053277@kiit.ac.in>
2025-05-12 10:38:17 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SudokuSolver {
+    int[][] sudoku = new int[9][9];
+
+        Scanner scn = new Scanner(System.in);
+        void input(){
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    sudoku[i][j] = scn.nextInt();
+                }
+            }
+        }
+        void show(){
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    System.out.print(sudoku[i][j] + "  ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        SudokuSolver(){};
+        SudokuSolver(int[][] sudoku){
+            this.sudoku = sudoku;
+        }
+
+}
